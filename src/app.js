e184d65d419a0547cdb1c32009ed37f8f081c9bd
kos5667 <kos5667@gmail.com>
2024-10-06 21:12:52 +0900

    Initialize Application Setting

--- src/app.js
+++ src/app.js
@@ -1,1 +1,50 @@
-"use strict";
+const express = require('express');
+const async = require('async');
+const { createClient } = require('redis')
+
+/**
+ * isRedisClient: {true, false} // Redis Client 서버
+ * isRedisServer: {true, false} // Redis 서버
+ *
+ * @type {undefined}
+ */
+const configurationOptions = {
+    isRedisClient: false,   // Redis Client
+    isRedisServer: false    // Redis Server
+};
+
+/**
+ * Initialize Application
+ */
+async.waterfall([
+    function (next) {
+        console.info('┌────────────────────────────────────────────────────────┐')
+        console.info('│            Redis Server is up and running!!            │')
+        console.info('└────────────────────────────────────────────────────────┘')
+        next(null);
+    },
+
+    /**
+     * Load Configuration
+     * @param next
+     */
+    function LoadConfiguration (next) {
+        for (let i = 1; i < process.argv.length; i++) {
+            switch (process.argv[i]) {
+                case '-redis-server':
+                    configurationOptions.isRedisServer = true;
+                    break;
+                case '-redis-client':
+                    configurationOptions.isRedisClient = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+        next(null);
+    },
+
+    function CreateRedisClient() {
+
+    }
+])
